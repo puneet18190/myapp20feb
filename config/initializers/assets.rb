39980f1810566e046e089b38ee6418bbe846a2d5
jalen201 <jalenjack201@hotmail.com>
2017-11-09 20:50:11 -0500

    organized code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( admin.js admin.css home.css media-queries.css buy.css particles.js home.js contact.css contact.js )
