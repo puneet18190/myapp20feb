7056c423baffdb321e5191fbcc2682831c8d5496
jalen201 <jalenjack201@hotmail.com>
2017-11-10 19:00:00 -0500

    changed video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,4 @@
 class Video < ApplicationRecord
-  has_attached_file :mp4,
-                    :styles => {:thumb => ["400x400#", :jpg]}
+  has_attached_file :mp4, :s3_protocol => 'https', :styles => {:thumb => ["400x400#", :jpg]}
   validates_attachment_content_type :mp4, content_type: /\Avideo/
 end
